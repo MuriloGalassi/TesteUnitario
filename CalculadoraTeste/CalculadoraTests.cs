e1bd4c2cde8415e0db705b8913fd7e3e4cf97fd1
MuriloGalassi <murilo.galassi@hotmail.com>
2025-01-27 15:43:59 -0300

    test Pronto

--- CalculadoraTeste/CalculadoraTests.cs
+++ CalculadoraTeste/CalculadoraTests.cs
@@ -1,10 +1,27 @@
-﻿namespace CalculadoraTeste;
+﻿using Calculadora.Services;
+
+namespace CalculadoraTeste;
 
 public class CalculadoraTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private CalculadoraImp _calc;
 
+    public CalculadoraTests()
+    {  
+        _calc = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void DeveSomar5Com10Eretornar15()
+    {
+        //Arrenge
+        int num1 = 5;
+        int num2 = 10;
+
+        //Act
+        int resultado = _calc.Somar(num1, num2);
+
+        //Assert
+        Assert.Equal(15, resultado);
     }
 }
